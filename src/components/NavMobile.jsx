7dc8047fa481741e854145be7b57ee720cbc3d72
Federico Luna <federicolun4@gmail.com>
2023-01-23 20:44:07 -0300

    Added navbar for mobile

---
+++ src/components/NavMobile.jsx
@@ -0,0 +1,50 @@
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerOverlay, Image, Stack, useDisclosure } from '@chakra-ui/react'
+import React from 'react'
+import { Menu } from './Menu'
+
+/* eslint-disable react/react-in-jsx-scope */
+export const NavMobile = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+  return (
+    <>
+      <Button
+        colorScheme='secondary'
+        ref={btnRef}
+        onClick={onOpen}
+        p={2}
+      >
+        <Image
+          src='images/icons/hamburger-menu.svg'
+          boxSize={35}
+        />
+      </Button>
+      <Drawer
+        size='xs'
+        placement='top'
+        isOpen={isOpen}
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        >
+        <DrawerOverlay />
+        <DrawerContent
+          bg='secondary.500'
+        >
+          <DrawerCloseButton
+            color='white'
+            size={'lg'}
+          />
+          <DrawerBody>
+              <Stack
+                align='center'
+                mt={10}
+                gap={5}
+              >
+                <Menu onClose={onClose}/>
+              </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
