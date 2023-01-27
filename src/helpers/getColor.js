fe9be0c6d0085cbc05ebad77a56670e8a4d94f69
Federico Luna <federicolun4@gmail.com>
2023-01-26 22:24:16 -0300

    Add helper to get colors

---
+++ src/helpers/getColor.js
@@ -0,0 +1,20 @@
+export const getTechnologyColor = (technology) => {
+  switch (technology) {
+    case 'React':
+      return 'linkedin'
+    case 'HTML':
+      return 'orange'
+    case 'CSS':
+      return 'blue'
+    case 'Javascript':
+      return 'yellow'
+    case 'Tailwind':
+      return 'cyan'
+    case 'Chakra UI':
+      return 'cyan'
+    case 'MongoDB':
+      return 'green'
+    case 'Bootsrap':
+      return 'purple'
+  }
+}
