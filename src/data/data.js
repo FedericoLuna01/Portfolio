3aca1670a9b5155392c23e3e22f27bdd7e3d676b
Federico Luna <federicolun4@gmail.com>
2023-01-28 11:46:57 -0300

    Added projets info

---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'Heroes App',
+    description: 'Esta aplicación fue desarrollada con el curso de Udemy de React de Fernando Herrera. La aplicación permite buscar a los personajes de Marvel y DC desde un archivo con datos y ver sus detalles. El proyecto fue desarrollado con React, Tailwind y React router dom. Contiene rutas protegidas y un login aunque sin necesidad de crear una cuenta',
+    technologies: ['React', 'Tailwind', 'React Router DOM'],
+    thumbnail: '/images/heroes-app-1.png',
+    carousel_images: ['/images/heroes-app-1.png', '/images/heroes-app-2.png', '/images/heroes-app-3.png'],
+    web: '',
+    github: 'https://github.com/FedericoLuna01/HeroesApp'
+  },
+  {
+    id: 2,
+    name: 'Buscador de Recetas',
+    description: 'Este es mi portfolio, diseñado por mí mismo, con el fin de mostrar mis habilidades como desarrollador web. El proyecto fue dessarrollado principalmente con React y ChakraUI',
+    technologies: ['HTML', 'Javascript', 'Bootsrap'],
+    thumbnail: '/images/BuscadorRecetas-1.png',
+    carousel_images: ['/images/BuscadorRecetas-1.png', '/images/BuscadorRecetas-2.png', '/images/BuscadorRecetas-3.png', '/images/BuscadorRecetas-4.png'],
+    web: '',
+    github: ''
+  },
+  {
+    id: 3,
+    name: 'Asistente de Pacientes',
+    description: 'asdojiajosd aosidj aoisjd aoisj aoisj doiajsod ijasoij daoijs oiaj iodjaiosj dioajs iodjaoisj dojaoijsjidasoj joa joisdjoa osida osijd asd',
+    technologies: ['React', 'Tailwind', 'MongoDB'],
+    thumbnail: '/images/APV-1.png',
+    carousel_images: ['/images/APV-1.png', '/images/APV-2.png', '/images/APV-3.png', '/images/APV-4.png'],
+    web: '',
+    github: ''
+  }
+]
+
+export const moreProjects = [
+  {
+    id: 1,
+    name: 'Cotizador de Crypto',
+    description: 'Este proyecto fue realizado en el curso de JavaScript de Juan Pablo de la Torre Valdez. La aplicación consume API donde permite cotizar el precio de las criptomonedas en tiempo real. El proyecto fue desarrollado con HTML y JavaScript Vanilla.',
+    technologies: ['HTML', 'Javascript', 'CSS'],
+    thumbnail: '/images/CotizadorCrypto-1.png',
+    carousel_images: ['/images/CotizadorCrypto-1.png', '/images/CotizadorCrypto-2.png'],
+    web: '',
+    github: ''
+  },
+  {
+    id: 2,
+    name: 'Gif App',
+    description: 'Esta aplicación fue desarrollada con el curso de Udemy de React de Fernando Herrera. La aplicación consume la API de Giphy y permite buscar los gifs que quieras. Tambien dejando la busqueda anterior en el historial.',
+    technologies: ['React', 'Tailwind'],
+    thumbnail: '/images/gif-app-1.png',
+    carousel_images: ['/images/gif-app-1.png', '/images/gif-app-2.png', '/images/gif-app-3.png'],
+    web: '',
+    github: ''
+  }
+]
