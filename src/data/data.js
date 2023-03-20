723b7f5006063f88b19f5936e3418118321dedfc
Federico Luna <federicolun4@gmail.com>
2023-03-20 05:14:25 -0300

    Added a new project

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,16 @@
 export const projects = [
   {
     id: 1,
+    name: 'All Nighter',
+    description: 'Esta página web se trata de una aplicación que permite buscar películas y ver sus detalles. El proyecto fue desarrollado con React, Chakra UI y React router dom. La api que se consume es de The Movie DB. Contiene una paginación, un buscador y un filtro por puntuación de estrellas.',
+    technologies: ['React', 'Chakra UI', 'React Router DOM'],
+    thumbnail: '/images/allnighter-1.png',
+    carousel_images: ['/images/allnighter-1.png', '/images/allnighter-2.png', '/images/allnighter-3.png', '/images/allnighter-4.png'],
+    web: 'https://moviesallnight.netlify.app/',
+    github: 'https://github.com/FedericoLuna01/AllNighterMovies'
+  },
+  {
+    id: 2,
     name: 'Heroes App',
     description: 'Esta aplicación fue desarrollada con el curso de Udemy de React de Fernando Herrera. La aplicación permite buscar a los personajes de Marvel y DC desde un archivo con datos y ver sus detalles. El proyecto fue desarrollado con React, Tailwind y React router dom. Contiene rutas protegidas y un login aunque sin necesidad de crear una cuenta',
     technologies: ['React', 'Tailwind', 'React Router DOM'],
@@ -10,7 +20,7 @@ export const projects = [
     github: 'https://github.com/FedericoLuna01/HeroesApp'
   },
   {
-    id: 2,
+    id: 3,
     name: 'Countries Rest',
     description: 'Esta aplicación es un challenge de FrontEndMentor, donde se consume la api de Rest Countries. Se permite buscar países, filtrarlos por region y ver sus detalles. El proyecto fue desarrollado con React y Chakra UI.',
     technologies: ['React', 'Chakra UI'],
@@ -18,16 +28,6 @@ export const projects = [
     carousel_images: ['/images/CountriesRest-1.png', '/images/CountriesRest-2.png', '/images/CountriesRest-3.png', '/images/CountriesRest-4.png'],
     web: 'https://findyourcontry.netlify.app',
     github: 'https://github.com/FedericoLuna01/CountriesApp'
-  },
-  {
-    id: 3,
-    name: 'Asistente de Pacientes',
-    description: 'Esta aplicación fue creada en el curso de JavaScript de Juan Pablo de la Torre Valdez. La aplicación permite crear, editar y eliminar pacientes. El proyecto fue desarrollado con React, Tailwind. Contiene rutas protegidas y un login, donde cada persona puede crear una cuenta y guardar sus pacientes. En cuanto al BackEnd está desarrollado con express y mongoDB.',
-    technologies: ['React', 'Tailwind', 'MongoDB'],
-    thumbnail: '/images/APV-1.png',
-    carousel_images: ['/images/APV-1.png', '/images/APV-2.png', '/images/APV-3.png', '/images/APV-4.png'],
-    web: null,
-    github: 'https://github.com/FedericoLuna01/APV-FrontEnd'
   }
 ]
 
@@ -43,7 +43,17 @@ export const moreProjects = [
     github: 'https://github.com/FedericoLuna01/Cryptocurrencies'
   },
   {
-    id: 2,
+    id: 3,
+    name: 'Asistente de Pacientes',
+    description: 'Esta aplicación fue creada en el curso de JavaScript de Juan Pablo de la Torre Valdez. La aplicación permite crear, editar y eliminar pacientes. El proyecto fue desarrollado con React, Tailwind. Contiene rutas protegidas y un login, donde cada persona puede crear una cuenta y guardar sus pacientes. En cuanto al BackEnd está desarrollado con express y mongoDB.',
+    technologies: ['React', 'Tailwind', 'MongoDB'],
+    thumbnail: '/images/APV-1.png',
+    carousel_images: ['/images/APV-1.png', '/images/APV-2.png', '/images/APV-3.png', '/images/APV-4.png'],
+    web: null,
+    github: 'https://github.com/FedericoLuna01/APV-FrontEnd'
+  },
+  {
+    id: 3,
     name: 'Gif App',
     description: 'Esta aplicación fue desarrollada con el curso de Udemy de React de Fernando Herrera. La aplicación consume la API de Giphy y permite buscar los gifs que quieras. Tambien dejando la busqueda anterior en el historial.',
     technologies: ['React', 'Tailwind'],
@@ -53,7 +63,7 @@ export const moreProjects = [
     github: 'https://github.com/FedericoLuna01/GifApp'
   },
   {
-    id: 3,
+    id: 4,
     name: 'Buscador de Recetas',
     description: 'Esta aplicación fue desarrollada en el curso de Udemy de JavaScript de Juan Pablo de la Torre Valdez. La aplicación consume la API de TheMealDB y permite buscar recetas de comida. El proyecto fue desarrollado con JavaScript Vanilla y Bootsrap.',
     technologies: ['HTML', 'Javascript', 'Bootsrap'],
@@ -63,7 +73,7 @@ export const moreProjects = [
     github: 'https://github.com/FedericoLuna01/BuscadorDeRecetas'
   },
   {
-    id: 4,
+    id: 5,
     name: 'URL Shortener',
     description: 'Este proyecto es un Challenge de la página FrontEndMentor. La aplicación consume una API que permite crear un link corto a partir de un link largo. La aplicatión fue desarrollada con React y Chakra UI.',
     technologies: ['React', 'Chakra UI'],
